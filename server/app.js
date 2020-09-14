bf98197765b50b0882f3230481c5b484cfa306d9
unknown <domgjonikrist@gmail.com>
2020-09-14 12:31:40 +0200

    connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+// connect to mlab database
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://krist:AoVjYlKMafRP8LjB@cluster0.bnfo8.gcp.mongodb.net/graphql?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
