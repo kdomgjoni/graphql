92e1bfa926864e5fe8f4eecb9f13857590c8c49d
unknown <domgjonikrist@gmail.com>
2020-09-14 10:54:46 +0200

    GraphQL Lists

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,7 +19,8 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -42,7 +43,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books, { authorId: parent.id});
+            }
+        }
     })
 });
 
